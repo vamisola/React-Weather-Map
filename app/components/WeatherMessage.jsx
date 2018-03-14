01b9bf5515a9a2a3006a447c02a9f3b0458a1692
Vina Compson <vv.compson@icloud.com>
2018-03-14 12:26:03 +1100

    Modify WeatherMessage

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,7 +1,6 @@
 var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
-  const { lon, lat } = location.city.coord;
   return (
     <div>
       <h3>It's {temp} in {location}</h3>
