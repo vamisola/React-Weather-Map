140c604f506e6962e97db9ff5d528127cbe3149e
Vina Compson <vv.compson@icloud.com>
2018-03-14 22:52:43 +1100

    update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 
+
 // var About= React.createClass({
 //   render: function(){
 //
@@ -9,8 +10,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>Welcome to About Page!</p>
+      <h1 className="text-center">About Page</h1>
+      <p>This is a weather application build on ReactJS. I have built this by following The Complete React Web App Developer Course from Udemy by Andrew Mead.</p>
+
+      <p>Here are some the toolds I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react"> React</a> - This was the Javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+      <p>You can clone the github repo here: <a href="https://github.com/vamisola/React-Weather-Map.git">https://github.com/vamisola/React-Weather-Map.git</a></p>
     </div>
   )
 };
